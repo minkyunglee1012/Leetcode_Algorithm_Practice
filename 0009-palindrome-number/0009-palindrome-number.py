3e363456a818b48a286ff06879200bbb56a0463f
Lee Min Kyung <minkyunglee1012@gmail.com>
2024-01-25 17:15:20 +0900

    Time: 40 ms (98.02%), Space: 16.5 MB (62.40%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        palindrome_num = str(x)
+        
+        if palindrome_num == palindrome_num[::-1]:
+            return True
+        return False
