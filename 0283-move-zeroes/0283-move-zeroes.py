138dfee21181d23c91931a0e8e68d9c6d2d885a0
Lee Min Kyung <minkyunglee1012@gmail.com>
2024-01-27 17:23:37 +0900

    Time: 738 ms (13.01%), Space: 18.1 MB (58.04%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,15 +4,11 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         zero_count = 0
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                zero_count += 1
-        
-        for j in range(zero_count):
+        while 0 in nums:
             nums.remove(0)
+            zero_count += 1
             
-        
-        for j in range(zero_count):
+        for i in range(zero_count):
             nums.append(0)
             
         return nums
