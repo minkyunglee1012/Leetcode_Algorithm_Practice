5c2da1d0d61d8f419c37e90e186c2771c78a9ffc
민경 <minkyunglee1012@gmail.com>
2024-03-25 14:09:03 +0900

    Leetcode2

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,10 +1,10 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        set_num1 = list(set(nums1))
-        set_num2 = list(set(nums2))
         common = []
-        for i in range(len(set_num1)):
-            if set_num1[i] in set_num2:
-                common.append(set_num1[i])
-                
-        return common
+
+        for i in range(len(nums1)):
+            if nums1[i] in nums2:
+                common.append(nums1[i])
+
+
+        return list(set(common))
--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,9 +1,18 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
+        num_list = []
+        for i in range(n):
+            num_list.append(i+1)
+        
         if n == 1:
             return 1
         
-        for i in range(1, n+1):
-            if sum([k for k in range(1, i+1)]) == sum([p for p in range(i, n+1)]):
-                return i
-        return -1
+        result = 0
+        for j in range(1, len(num_list)):
+            if sum(num_list[:j]) == sum(num_list[j - 1:]):
+                result = num_list[j-1]
+  
+        if result == 0:
+            result = -1
+            
+        return result
