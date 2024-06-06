8691288ce3ebd9f47a6e962c6025b7e305300de7
Lee Min Kyung <minkyunglee1012@gmail.com>
2024-06-06 16:16:07 +0900

    Time: 71 ms (39.41%), Space: 16.7 MB (88.91%) - LeetHub

---
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.py
@@ -0,0 +1,11 @@
+class Solution:
+    def vowelStrings(self, words: List[str], left: int, right: int) -> int:
+        vowel = "aeiou"
+        output = 0
+
+        for i in range(left, right+1):
+            if words[i][0] in vowel and words[i][-1] in vowel:
+                # print(words[i])
+                output += 1
+
+        return output
