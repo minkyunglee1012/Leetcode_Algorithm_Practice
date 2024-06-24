36eac0343e7ad998fdbe2ae12116299cd3ec938f
Lee Min Kyung <minkyunglee1012@gmail.com>
2024-06-24 09:46:35 +0900

    Time: 32 ms (85.02%), Space: 16.5 MB (26.88%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        while (len(str(n)) > 1):
+            s = 0
+            for i in str(n):
+                s += int(i) ** 2
+            n = s
+        
+        if n == 1 or n == 7:
+            return True
+        return False
+        
+   
+        
