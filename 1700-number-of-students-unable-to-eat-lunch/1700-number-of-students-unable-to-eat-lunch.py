b68c31a9041591b4f8496f8e23b53fe2ed8f0e6a
Lee Min Kyung <minkyunglee1012@gmail.com>
2024-07-07 22:57:32 +0900

    1700-number-of-students-unable-to-eat-lunch.py 업데이트

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,6 +1,8 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        
         while students and sandwiches[0] in students:
+            
             if students[0] != sandwiches[0]:
                 pop = students.pop(0)
                 students.append(pop)
