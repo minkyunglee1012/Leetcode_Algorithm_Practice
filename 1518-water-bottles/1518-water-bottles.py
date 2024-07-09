8f6e142bb8d0a865fd47c8cf142bc771549e23a3
Lee Min Kyung <minkyunglee1012@gmail.com>
2024-07-09 09:44:46 +0900

    Time: 27 ms (92.03%), Space: 16.5 MB (38.21%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+
+        bottles = numBottles
+        if numBottles < numExchange:
+            return bottles
+        
+        while numBottles >= numExchange:
+            bottles += numBottles // numExchange
+            numBottles = numBottles//numExchange + numBottles%numExchange
+
+            
+        return bottles
