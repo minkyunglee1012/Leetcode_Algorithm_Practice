9a02f483c2eed171f382dedf84dba4288e05acd6
Lee Min Kyung <minkyunglee1012@gmail.com>
2024-09-10 11:20:29 +0900

    0441-arranging-coins.py

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,7 +1,6 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        if n == 1:
-            return 1
+        if n == 1: return 1
         
         count = 0
         for i in range(1, n):
